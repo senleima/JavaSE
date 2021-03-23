f850673a4b812efacf0e7febcc842ab795e45080
senleima <senleima@163.com>
2021-03-24 01:16:58 +0800

    first commit

---
+++ code/Base/HelloWorld.java
@@ -0,0 +1,26 @@
+
+public class HelloWorld{
+	
+	public static void main(String aabb[]){
+		System.out.println("Hello World!");
+	}
+}
+
+
+/*
+1、使用命令行的方式执行的时候，cmd的默认编码格式是GBK，在输入中文的时候需要设置文件的编码格式为ANSI，不会出现乱码错误
+2、java文件的名称必须与public class的名称保持一致
+3、一个java文件中可以包含多个class,但public class只能有1个
+4、public static void main(String[] args)是所有java程序的入口，若执行对应的java代码，则必须要添加如下方法，且格式是固定的
+5、main方法中参数列表可以支持多种写法：String[] args,String [] args,String args[]
+6、main方法中参数的名称无所谓，但一般写成args
+7、java代码在编写时，每行结尾时需要使用;结束
+8、java代码的代码块需要使用{}括起来，前后匹配
+9、java中的注释分为三类：
+*/
+	//单行注释
+	/*多行注释*/
+	/**	
+	  文档注释
+	*/
+
